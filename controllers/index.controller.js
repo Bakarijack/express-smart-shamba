a2f2391be358245947c3ba48b2ed1564db33e528
Bakarijack <bakarikilu254@gmail.com>
2023-03-12 10:17:26 +0300

    login functionality ok

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,6 +1,8 @@
 
 exports.indexPageRender = (req, res, next) => res.render('index')
 
+exports.loginPageRender = (req, res, next) => res.render('login', { layout: 'auth'})
+
 exports.signupPageRender = (req, res, next) => res.sendFile('public/Login.html', {root: '.'})
 
 
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,6 +2,8 @@ const indexController = require('../controllers/index.controller')
 
 module.exports = app => {
     app.get('/', indexController.indexPageRender)
+
+    app.get('/login',indexController.loginPageRender)
     
     app.get('/signup', indexController.signupPageRender)
 
