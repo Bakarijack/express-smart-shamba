d9d8c1093338e7782c0a4c30c1fbbf42552b9918
Bakarijack <bakarikilu254@gmail.com>
2023-03-12 11:09:33 +0300

    registration functionality ok

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -3,7 +3,7 @@ exports.indexPageRender = (req, res, next) => res.render('index')
 
 exports.loginPageRender = (req, res, next) => res.render('login', { layout: 'auth'})
 
-exports.signupPageRender = (req, res, next) => res.sendFile('public/Login.html', {root: '.'})
+exports.signupPageRender = (req, res, next) => res.render('signup', { layout: 'auth'})
 
 
 exports.dashboardPageRender = (req, res, next) => res.sendFile('public/Dashboard.html', { root: '.'})
