2f3d8e168e6e17bf3ade2bee3037e6922fa54baa
Bakarijack <bakarikilu254@gmail.com>
2023-03-13 16:56:15 +0300

    land registration section A

--- express.js
+++ express.js
@@ -10,7 +10,9 @@ app.use(express.static('public'))
 
 app.engine('handlebars', expressHandlebars.engine({
     extname: 'handlebars',
-    defaultLayout: 'main'
+    defaultLayout: 'main',
+    layoutsDir: __dirname+'/views/layouts/',
+    partialsDir: __dirname+ '/views/partials/'
 }))
 
 app.set('view engine', 'handlebars')
